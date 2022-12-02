df41ba65f44fac105652ba2bdd52226fd1cf2fd9
Malin Andersson <annamalinelisabet@gmail.com>
2022-12-02 15:21:48 +0100

    calender

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,63 @@
-import React from 'react'
+import {useState} from 'react'
 import './Hero.css'
+import { FaAngleRight, FaAngleLeft } from 'react-icons/fa'
+import { GiWheat } from 'react-icons/gi'
+import 'animate.css'
 
 const Hero = () => {
+
+  const [showCalendar, setShowCalendar] = useState(false)
+
   return (
     <div className='Hero'>
-        <div className="hero-bg"></div>
+        <div className="hero-bg">
+
+          {/*------ CALENDAR -------*/}
+          <div className={`calendar ${showCalendar ? `show` : ``}`}>
+            <div className="calendar-wrapper">
+              <div className="calendar-container">
+                <div className="header-div">
+                  <h3 className='calendar-header'>Fira mumsiga fikadagar med oss</h3>
+                  <p className='week'>vecka 49</p>
+                </div>
+                <div className="flex-div">
+                  <ul className='days-list'>
+                    <li className='day'>måndag</li>
+                    <li className='day'>tisdag</li>
+                    <li className='day'>onsdag</li>
+                    <li className='day'>torsdag</li>
+                    <li className='day'>fredag</li>
+                    <li className='day'>lördag</li>
+                    <li className='day'>söndag</li>
+                  </ul>
+                  <ul className='info-list'>
+                    <li className='info'></li>
+                    <li className='info'><a className='info' href="https://www.instagram.com/p/Ckq2rJkDB4t/">Tisdagsmunken - Äpple</a></li>
+                    <li className='info'></li>
+                    <li className='info'></li>
+                    <li className='info'><a className='info' href="https://www.instagram.com/p/ClZIoqEjsq2/">Pepparkakans dag</a></li>
+                    <li className='info'></li>
+                    <li className='info'><a className='info' href="https://www.instagram.com/p/CWva1ChrP4I/">Tredje advent</a></li>
+                  </ul>
+
+                </div>
+              </div>
+
+
+
+
+              <GiWheat className='wheat'/>
+              <div className={`open-close ${showCalendar ? `` : `animate__animated animate__heartBeat animate__repeat-3 animate__ slower animate__delay-2s`}`} onClick={() => setShowCalendar(state => !state)}>
+                { showCalendar 
+                  ? <FaAngleLeft className='arrow'/>
+                  : <FaAngleRight className='arrow'/>
+                }
+              </div>
+
+            </div>
+          </div>
+        </div>
+
     </div>
   )
 }
