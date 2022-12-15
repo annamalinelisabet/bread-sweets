ec2f947981d6ec654bd194088860b67ffafaa5c4
Anna <annaelg@live.se>
2022-12-15 14:41:53 +0100

    push

--- src/Views/Products/ProductsView.jsx
+++ src/Views/Products/ProductsView.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import NewProductCard from '../../Components/NewProductCard/NewProductCard';
 import './ProductsView.css'
 
