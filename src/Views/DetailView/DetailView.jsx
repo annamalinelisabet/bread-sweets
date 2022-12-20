9cf3964110ac2cd9012cea083bd73eed68e024de
Malin Andersson <annamalinelisabet@gmail.com>
2022-12-20 17:20:54 +0100

    detail props

--- src/Views/DetailView/DetailView.jsx
+++ src/Views/DetailView/DetailView.jsx
@@ -2,26 +2,32 @@ import './DetailView.css'
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { FaArrowLeft } from 'react-icons/fa'
-// import { ImSpoonKnife } from 'react-icons/im'
-import buns from '../../Images/buns.png'
+import { ImSpoonKnife } from 'react-icons/im'
+// import buns from '../../Images/buns.png'
 import ContentCard from '../../Components/ContentCard/ContentCard'
 import AllergenCard from '../../Components/AllergenCard/AllergenCard'
-import { useParams } from 'react-router-dom'
-import products from '../../products.json'
+import { useParams, useLocation } from 'react-router-dom'
+// import products from '../../products.json'
 
 const DetailView = () => {
   const [showAll, setShowAll] = useState(false);
 
-  const { id } = useParams()
+//   const { id } = useParams()
+  const location = useLocation()
+  const { product } = location.state
 
-  const [product, setProduct] = useState('')
+//   const [product, setProduct] = useState('')
 
-  console.log(products)
-  useEffect(() => {
-    setProduct(products.filter(item => item.id === id))
-    console.log(product)
+//   console.log(products)
+//   useEffect(() => {
+//     setProduct(products.filter(item => item.id === id))
+//     console.log(product)
     
-}, [products])
+// }, [products])
+
+    useEffect(() => {
+        window.scrollTo({top: 0, left: 0, behavior: 'instant'});
+    }, [])
 
   return (
     <div className='DetailView frame'>
@@ -36,8 +42,8 @@ const DetailView = () => {
                 <div className='wrapper'>
 
                 <div className='img-wrapper'>
-                    <img src={buns} alt="" />
-                    {/* { img ? <img src={buns} alt="food"/> : <ImSpoonKnife className='spork'/> } */}
+                    {/* <img src={buns} alt="" /> */}
+                    { product.img ? <img src={product.img} alt="food"/> : <ImSpoonKnife className='spork'/> }
                 </div>
 
                 <div className='text-wrapper'>
@@ -52,7 +58,7 @@ const DetailView = () => {
                     <div className='dflex mt-5 wrap'>
                         <div className='price-wrap'>
                             <p>Från</p>
-                            <p className='ml price'>100 :-</p>
+                            <p className='ml price'>{product.price} kr</p>
                         </div>
                             <button className='btn btn-add'>Lägg till</button>
                     </div>
