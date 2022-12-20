10a2ae135ad708dfd6b5989b79ef68c0d6434012
Malin Andersson <annamalinelisabet@gmail.com>
2022-12-20 11:52:24 +0100

    active category m.m

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -1,16 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import './Filter.css'
 import categories from '../../categories.json'
 
-const Filter = ({setCategory}) => {
+const Filter = ({setCategory, setAllergy, setName, setDesc }) => {
     const [active, setActive] = useState('')
     
-    const handleClick = (category) => {
+    const handleClick = (category) => {      
         console.log(category)
         setCategory(category.id)
         setActive(category)
+        setAllergy('')
+        setName(category.name)
+        setDesc(category.desc)
+        document.getElementById("cat").classList.remove('active')
     }
+
+    useEffect(() => {
+        categories.filter(category => category.id === 1).map(category => 
+            document.getElementById("cat").classList.add('active'))
+            setName('Populär')
+            setDesc('Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, aliquid?')
+    }, [])
+    
 
   return (
     <div className='Filter'>
@@ -19,8 +31,7 @@ const Filter = ({setCategory}) => {
                     {
                     categories.map(category => (
                             <li key={category.id}>
-                                {/* <Link className={`category ${category.highlight === true ? 'highLight' : active === category && 'active'}`} to="#" onClick={() => handleClick(category)}> */}
-                                <Link className={`category ${active === category ? 'active' : category.highlight === true && 'highLight'}`} to="#" onClick={() => handleClick(category)}>
+                                <Link id='cat' className={`category ${active === category ? 'active' : category.highlight === true && 'highLight'}`} to="#" onClick={() => handleClick(category)}>
                                 <span className='nobreak'>{category.name}</span>
                                 </Link>
                             </li>
--- src/Views/Products/ProductsView.jsx
+++ src/Views/Products/ProductsView.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import NewProductCard from '../../Components/NewProductCard/NewProductCard';
 import './ProductsView.css'
 import Filter from '../../Components/Filter/Filter'
@@ -6,15 +6,10 @@ import products  from '../../products.json'
 
 const ProductsView = () => {
 
-    const navref = useRef()
-
-    const [active, setActive] = useState('')
-
-    const [gluten, setGluten] = useState(false)
-    const [lactose, setLactose] = useState(false)
-    const [vegan, setVegan] = useState(false)
-
+    const [allergy, setAllergy] = useState('')
     const [category, setCategory] = useState(1)
+    const [name, setName] = useState('')
+    const [desc, setDesc] = useState('')
 
     const [popular, setPopular] = useState([])
     const [filtred, setFiltred] = useState([])
@@ -30,94 +25,50 @@ const ProductsView = () => {
     }, [category])
 
 
-    useEffect(() => {   
-        if(gluten) {
-            setFiltred(products.filter(product => product.category === category && product.gluten === true))  
-            setPopular(products.filter(product => product.tag === 'popular' && product.gluten === true)) 
-            setLactose(false)
-            setVegan(false)
-        }
-        else {
+    const handleClick = (click) => {
+        if(click === allergy){
+            setAllergy('')
             setFiltred(products.filter(product => product.category === category)) 
             setPopular(products.filter(product => product.tag === 'popular'))
         }
-    }, [gluten])
-
-    useEffect(() => {   
-        if(lactose) {
-            setFiltred(products.filter(product => product.category === category && product.lactose === true))
-            setPopular(products.filter(product => product.tag === 'popular' && product.lactose === true))    
-            setGluten(false)
-            setVegan(false)
-        }
         else {
-            setFiltred(products.filter(product => product.category === category)) 
-            setPopular(products.filter(product => product.tag === 'popular'))
+            setAllergy(click)
+            setFiltred(products.filter(product => product.category === category && product.allergy === click))  
+            setPopular(products.filter(product => product.tag === 'popular' && product.allergy === click)) 
         }
-    }, [lactose])
-
-    useEffect(() => {   
-        if(vegan) {
-            setFiltred(products.filter(product => product.category === category && product.vegan === true))
-            setPopular(products.filter(product => product.tag === 'popular' && product.vegan === true))    
-            setLactose(false)
-            setGluten(false)
-        }
-        else {
-            setFiltred(products.filter(product => product.category === category)) 
-            setPopular(products.filter(product => product.tag === 'popular'))
-        }
-    }, [vegan])
+    }
 
 
   return (
     <div className='ProductsView frame'>
         <div className="container">
-            <Filter setCategory={setCategory}/>
+            <Filter setCategory={setCategory} setAllergy={setAllergy} setName={setName} setDesc={setDesc}/>
+            <div className="flex-div">
+                <div className='allergies-wrap'>
 
-            <div className='allergies-wrap'>
+                    <div className={`allergies-icon gluten ${allergy === 'gluten' ? 'active' : ''}`} onClick={() => handleClick('gluten')}>G<p className='tooltiptext'>Gluten</p></div>
+                    <div className={`allergies-icon lactose ${allergy === 'lactose' ? 'active' : ''}`} onClick={() => handleClick('lactose')}>L<p className='tooltiptext'>Laktos</p></div>
+                    <div className={`allergies-icon vegan ${allergy === 'vegan' ? 'active' : ''}`} onClick={() => handleClick('vegan')}>V<p className='tooltiptext'>Vegan</p></div>
 
-
-                <div ref={navref} className='allergies-icon gluten' onClick={() => setGluten(state => !state)}>G<p className='tooltiptext'>Gluten</p></div>
-                <div ref={navref} className='allergies-icon lactose' onClick={() => setLactose(state => !state)}>L<p className='tooltiptext'>Laktos</p></div>
-                <div ref={navref} className='allergies-icon vegan' onClick={() => setVegan(state => !state)}>V<p className='tooltiptext'>Vegan</p></div>
-
-            </div>
-            { gluten && !filtred.length && <p className='info-text'>Tyvärr har vi inget glutenfritt i denna kategori</p> }
-            { lactose && !filtred.length && <p className='info-text'>Tyvärr har vi inget laktosfritt i denna kategori</p> }
-            { vegan && !filtred.length && <p className='info-text'>Tyvärr har vi inget veganskt i denna kategori</p> }
-            {
-                category === 1 ? 
-                <div className='category-wrapper'>
-
-                    {  popular.map(product => 
-                        <NewProductCard key={product.id} product={product} />
-
-                    {/* { products.filter(product => product.tag === 'popular').map(product => 
-                        <NewProductCard key={product.id} product={product} />
-                    )
-                    } */}
-                    {  
-                    popular.sort((a) => a.tag === 'new').map(product => 
-                        <NewProductCard key={product.id} product={product}   />
-
-                    )
-                    }
                 </div>
+                <div className='header-div'>
+                    <h1>{name}</h1>
+                    <p>{desc}</p>
+                </div>
+            </div>
+            { allergy === 'gluten' && !filtred.length && <p className='info-text'>Tyvärr har vi inget glutenfritt i denna kategori</p> }
+            { allergy === 'lactose' && !filtred.length && <p className='info-text'>Tyvärr har vi inget laktosfritt i denna kategori</p> }
+            { allergy === 'vegan' && !filtred.length && <p className='info-text'>Tyvärr har vi inget veganskt i denna kategori</p> }
+            {
+                category === 1 
+                ? 
+                <div className='category-wrapper'>
+                    {  popular.map(product => <NewProductCard key={product.id} product={product} />)  }
+                </div>
+
                 :
                 <div className='category-wrapper'>
-
-                    { filtred.map(product => 
-
-                    {/* { products.filter(product => product.category === category).map(product => 
-                        <NewProductCard key={product.id} product={product}/>
-                    )
-                    } */}
-                    { filtred.sort(product => product.tag === 'new').map(product => 
-
-                        <NewProductCard key={product.id} product={product}/>
-                    )
-                    }
+                    { filtred.map(product => <NewProductCard key={product.id} product={product}/>) }
                 </div>
             }
         </div>
